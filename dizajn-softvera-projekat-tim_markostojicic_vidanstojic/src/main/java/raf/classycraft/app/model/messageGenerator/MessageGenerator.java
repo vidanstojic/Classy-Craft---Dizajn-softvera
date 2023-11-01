1d51589dde2be26ed1d80aae7d14225f35a96fa0
markostojicic03 <mstojicic12923rn@raf.rs>
2023-11-01 20:33:56 +0100

    Metode MessageGenerator

--- dizajn-softvera-projekat-tim_markostojicic_vidanstojic/src/main/java/raf/classycraft/app/model/messageGenerator/MessageGenerator.java
+++ dizajn-softvera-projekat-tim_markostojicic_vidanstojic/src/main/java/raf/classycraft/app/model/messageGenerator/MessageGenerator.java
@@ -5,12 +5,15 @@ import raf.classycraft.app.observer.ISubscriber;
 import raf.classycraft.app.observer.Notification;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class MessageGenerator implements IPublisher {
 
     private String tekst;
     private Tip tip;
     private LocalDateTime timestamp;
+
+    private List<ISubscriber> subscribers;
 
     public MessageGenerator(Tip tip, LocalDateTime timestamp){
         this.tip = tip;
@@ -23,18 +26,50 @@ public class MessageGenerator implements IPublisher {
 
     @Override
     public void addSubscriber(ISubscriber iSubscriber) {
-
+        this.subscribers.add(iSubscriber);
     }
 
     @Override
     public void removeSubscriber(ISubscriber iSubscriber) {
-
+        this.subscribers.remove(iSubscriber);
     }
 
     @Override
     public void notifySub(Notification notification) {
-
+        for(ISubscriber listener : subscribers){
+            listener.update(notification);
+        }
     }
 
- 
+    public String getTekst() {
+        return tekst;
+    }
+
+    public void setTekst(String tekst) {
+        this.tekst = tekst;
+    }
+
+    public Tip getTip() {
+        return tip;
+    }
+
+    public void setTip(Tip tip) {
+        this.tip = tip;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public List<ISubscriber> getSubscribers() {
+        return subscribers;
+    }
+
+    public void setSubscribers(List<ISubscriber> subscribers) {
+        this.subscribers = subscribers;
+    }
 }
