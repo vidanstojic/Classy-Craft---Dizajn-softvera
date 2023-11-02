a5cddf3b0d2faffacfc76e32894d432de5217691
markostojicic03 <mstojicic12923rn@raf.rs>
2023-11-02 11:02:03 +0100

    FileLogger

--- dizajn-softvera-projekat-tim_markostojicic_vidanstojic/src/main/java/raf/classycraft/app/model/simpleFactory/FileLogger.java
+++ dizajn-softvera-projekat-tim_markostojicic_vidanstojic/src/main/java/raf/classycraft/app/model/simpleFactory/FileLogger.java
@@ -3,6 +3,11 @@ package raf.classycraft.app.model.simpleFactory;
 import raf.classycraft.app.model.simpleFactory.Logger;
 import raf.classycraft.app.observer.Notification;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileLogger implements Logger {
 
 
@@ -13,6 +18,25 @@ public class FileLogger implements Logger {
 
     @Override
     public void log(String message) {
+        String filePath = "resources/txtFiles/log.txt";
+
+        File file = new File(filePath);
+
+        try {
+            FileWriter fileWriter = new FileWriter(file);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            bufferedWriter.write(message);
+            bufferedWriter.newLine();
+
+            bufferedWriter.close();
+            fileWriter.close();
+
+
+        } catch (IOException e) {
+            System.out.println("Podaci se nisu pravilno upisali u fajl");
+            throw new RuntimeException(e);
+        }
 
     }
 }
