0134e5a478cfd3afbe8d3202bbb8a48b1a4aa282
markostojicic03 <mstojicic12923rn@raf.rs>
2023-11-05 11:05:26 +0100

    Inicijalizacija Logger-a u AppFramework

--- dizajn-softvera-projekat-tim_markostojicic_vidanstojic/src/main/java/raf/classycraft/app/core/ApplicationFramework.java
+++ dizajn-softvera-projekat-tim_markostojicic_vidanstojic/src/main/java/raf/classycraft/app/core/ApplicationFramework.java
@@ -26,8 +26,9 @@ public class ApplicationFramework {
 
         classyRepository = new ClassyRepositoryImplemention();
         LoggerFactory loggerFactory = new LoggerFactory();
-        loggerFactory.createLogger("consoleLogger");
-        loggerFactory.createLogger("fileLogger");
+        // proveriti
+        consoleLogger = (ConsoleLogger) loggerFactory.createLogger("consoleLogger");
+        fileLogger = (FileLogger) loggerFactory.createLogger("fileLogger");
 
 
     }
