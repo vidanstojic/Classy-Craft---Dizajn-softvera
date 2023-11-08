0cecd7aed9b24c0e386d155ce4c2361668a43c9f
markostojicic03 <mstojicic12923rn@raf.rs>
2023-11-08 15:23:17 +0100

    Problem sa ucitavanjem ikonica

--- dizajn-softvera-projekat-tim_markostojicic_vidanstojic/src/main/java/raf/classycraft/app/gui/tree/view/ClassyTreeCellRenderer.java
+++ dizajn-softvera-projekat-tim_markostojicic_vidanstojic/src/main/java/raf/classycraft/app/gui/tree/view/ClassyTreeCellRenderer.java
@@ -1,12 +1,35 @@
 package raf.classycraft.app.gui.tree.view;
 
+import raf.classycraft.app.gui.tree.model.ClassyTreeItem;
+import raf.classycraft.app.model.compositeImplement.Project;
+import raf.classycraft.app.model.compositeImplement.ProjectExplorer;
+
 import javax.swing.*;
 import javax.swing.tree.DefaultTreeCellRenderer;
 import java.awt.*;
+import java.net.URL;
 
 public class ClassyTreeCellRenderer extends DefaultTreeCellRenderer {
     @Override
     public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel, boolean expanded, boolean leaf, int row, boolean hasFocus) {
         return super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf, row, hasFocus);
+
+        URL imageURL = null;
+
+
+        if (((ClassyTreeItem)value).getClassyNode() instanceof ProjectExplorer) {
+            imageURL = getClass().getResource("/images/projectExplorer.png");
+        }
+        else if (((ClassyTreeItem)value).getClassyNode() instanceof Project) {
+            imageURL = getClass().getResource("/images/project.png");
+        }
+
+        Icon icon = null;
+        if (imageURL != null)
+            icon = new ImageIcon(imageURL);
+        setIcon(icon);
+
+        return this;
+
     }
 }
