86761ed264f7f722fabe35748f74aa411d50b666
markostojicic03 <mstojicic12923rn@raf.rs>
2023-12-03 23:52:54 +0100

    Dodavanje prozora u AddClassState

--- dizajn-softvera-projekat-tim_markostojicic_vidanstojic/src/main/java/raf/classycraft/app/state/concrete/AddClassState.java
+++ dizajn-softvera-projekat-tim_markostojicic_vidanstojic/src/main/java/raf/classycraft/app/state/concrete/AddClassState.java
@@ -1,16 +1,35 @@
 package raf.classycraft.app.state.concrete;
 
+import raf.classycraft.app.gui.tree.factory.DiagramFactory;
+import raf.classycraft.app.gui.tree.factory.FactoryChild;
 import raf.classycraft.app.gui.view.DiagramView;
 import raf.classycraft.app.model.elementDiagram.concreteInterclass.ClassInterClass;
 import raf.classycraft.app.state.State;
 
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.MouseEvent;
 
 public class AddClassState implements State {
 
     @Override
     public void stateMousePressed(MouseEvent e, DiagramView tempTab) {
-        ClassInterClass classInterClass = new ClassInterClass();
+        Object[] selectionValues = {"Class", "Interface", "Enum"};
+        String initialSelection = "Class";
+        Object selection = JOptionPane.showInputDialog(null, "What element do you want to add?",
+                "Add new element", JOptionPane.QUESTION_MESSAGE, null, selectionValues, initialSelection);
+        if (selection == null) {
+            return;
+        }
+        else if (selection.equals("Class")) {
+            System.out.println("Dodavanje klase");
+        }
+        else if (selection.equals("Interface")) {
+            System.out.println("Dodavanje interfejsa");
+        }
+        else if (selection.equals("Enum")) {
+            System.out.println("Dodavanje enuma");
+        }
     }
 
     @Override
