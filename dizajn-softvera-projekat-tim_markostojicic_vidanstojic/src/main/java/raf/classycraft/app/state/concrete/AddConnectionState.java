10c65373cac86ecae5ab9f4f07cbeb0f8820df9c
vidanstojic <147335878+vidanstojic@users.noreply.github.com>
2023-12-04 11:51:55 +0100

    veze

--- dizajn-softvera-projekat-tim_markostojicic_vidanstojic/src/main/java/raf/classycraft/app/state/concrete/AddConnectionState.java
+++ dizajn-softvera-projekat-tim_markostojicic_vidanstojic/src/main/java/raf/classycraft/app/state/concrete/AddConnectionState.java
@@ -1,15 +1,40 @@
 package raf.classycraft.app.state.concrete;
 
 import raf.classycraft.app.gui.view.DiagramView;
+import raf.classycraft.app.gui.view.paint.ClassPainter;
+import raf.classycraft.app.model.elementDiagram.concreteInterclass.ClassInterClass;
 import raf.classycraft.app.state.State;
 
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.MouseEvent;
 
 public class AddConnectionState implements State {
 
     @Override
     public void stateMousePressed(MouseEvent e, DiagramView tempTab) {
-
+        Object[] selectionValues = {"Association", "Dependency", "Composition", "Aggregation", "Generalisation"};
+        String initialSelection = "Association";
+        Object selection = JOptionPane.showInputDialog(null, "What type of connection do you want to add?",
+                "Add new connection", JOptionPane.QUESTION_MESSAGE, null, selectionValues, initialSelection);
+        if (selection == null) {
+            return;
+        }
+        else if (selection.equals("Association")) {
+            System.out.println("Dodavanje klase");
+        }
+        else if (selection.equals("Dependency")) {
+            System.out.println("Dodavanje dependency");
+        }
+        else if (selection.equals("Composition")) {
+            System.out.println("Dodavanje kompozita");
+        }
+        else if (selection.equals("Aggregation")){
+            System.out.println("Dodavanje agregacije");
+        }
+        else if (selection.equals("Generalisation")) {
+            System.out.println("Dodavanje generalizacije");
+        }
     }
 
     @Override
