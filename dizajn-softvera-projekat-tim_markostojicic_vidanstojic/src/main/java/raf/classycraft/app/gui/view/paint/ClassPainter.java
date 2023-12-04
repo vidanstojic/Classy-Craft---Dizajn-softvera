e7f0e2d0d99fc6af58eb5c26b69bac54c7e585a4
vidanstojic <147335878+vidanstojic@users.noreply.github.com>
2023-12-04 16:43:16 +0100

    Pravljenje velicine klase po imenu

--- dizajn-softvera-projekat-tim_markostojicic_vidanstojic/src/main/java/raf/classycraft/app/gui/view/paint/ClassPainter.java
+++ dizajn-softvera-projekat-tim_markostojicic_vidanstojic/src/main/java/raf/classycraft/app/gui/view/paint/ClassPainter.java
@@ -3,7 +3,6 @@ package raf.classycraft.app.gui.view.paint;
 import raf.classycraft.app.model.elementDiagram.DiagramElement;
 import raf.classycraft.app.model.elementDiagram.concreteInterclass.ClassInterClass;
 
-import javax.swing.*;
 import java.awt.*;
 
 public class ClassPainter extends InterClassPainter{
@@ -17,12 +16,17 @@ public class ClassPainter extends InterClassPainter{
     @Override
     public void paint(Graphics2D graphics2D, DiagramElement diagramElement) {
         Stroke stroke = new BasicStroke(diagramElement.getStroke());
+        String string = classInterClass.getName();
+        Font font = new Font("Arial", Font.PLAIN, 10);
+        FontMetrics fontMetrics = graphics2D.getFontMetrics(font);
+        int length = fontMetrics.stringWidth(string);
         graphics2D.setStroke(stroke);
-        graphics2D.drawRect(point.x, point.y, 100,120);
+        graphics2D.drawRect(point.x, point.y, (int) (10 + length*1.25 + 10),120);
         graphics2D.drawString("C", point.x + 5, point.y + 15);
         graphics2D.drawString(classInterClass.getName(), point.x + 20, point.y + 15);
-        graphics2D.drawLine(point.x, point.y + 20, point.x + 100, point.y + 20);
+        graphics2D.drawLine(point.x, point.y + 20, point.x + (int) (10 + length*1.25 + 10), point.y + 20);
         //graphics2D.setBackground(Color.WHITE);
+        Rectangle rectangle = new Rectangle();
         
     }
 
