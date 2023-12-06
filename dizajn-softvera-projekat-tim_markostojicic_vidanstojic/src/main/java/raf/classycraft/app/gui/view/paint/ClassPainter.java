74a97ccd53288af53a21eeff06281b482ff6de7e
vidanstojic <147335878+vidanstojic@users.noreply.github.com>
2023-12-07 00:27:12 +0100

    ispravke

--- dizajn-softvera-projekat-tim_markostojicic_vidanstojic/src/main/java/raf/classycraft/app/gui/view/paint/ClassPainter.java
+++ dizajn-softvera-projekat-tim_markostojicic_vidanstojic/src/main/java/raf/classycraft/app/gui/view/paint/ClassPainter.java
@@ -24,6 +24,7 @@ public class ClassPainter extends InterClassPainter{
         String nameOfClass = classInterClass.getName();
         graphics2D.setColor(classInterClass.getColor());
         graphics2D.setStroke(stroke);
+        ///System.out.println("class y" + classInterClass.getPoint().y);
         point1.y = classInterClass.getPoint().y + 35;
 
         int length = graphics2D.getFontMetrics().stringWidth(nameOfClass);
@@ -47,12 +48,12 @@ public class ClassPainter extends InterClassPainter{
                 length  = graphics2D.getFontMetrics().stringWidth(attributeFullName);
             }
             graphics2D.drawString(attributeFullName,classInterClass.getPoint().x+5, point1.y);
-            point1.y = point1.y + 15;
+            point1.y = point1.y + 13;
 
         }
         Point pointForLine = new Point(classInterClass.getPoint().x, point1.y);
         point1.y = point1.y + 15;
-
+        ///System.out.println("pointforline " + pointForLine);
 
         for(Method method : classInterClass.getMethods()){
             String visibilityChar;
@@ -72,16 +73,17 @@ public class ClassPainter extends InterClassPainter{
             if(length < graphics2D.getFontMetrics().stringWidth(methodeFullName)){
                 length  = graphics2D.getFontMetrics().stringWidth(methodeFullName);
             }
-            graphics2D.drawString(methodeFullName,classInterClass.getPoint().x+5, point1.y);
+            graphics2D.drawString(methodeFullName,classInterClass.getPoint().x+5, point1.y - 10);
             point1.y = point1.y + 15;
         }
         if(classInterClass.getAttributes().size() >= 1){
-            graphics2D.drawLine(pointForLine.x, pointForLine.y, pointForLine.x+ (int) (15 + length + 15), pointForLine.y);
+           /// System.out.println("pointforline " + pointForLine);
+            graphics2D.drawLine(pointForLine.x, pointForLine.y - 10, pointForLine.x+ (int) (15 + length + 15), pointForLine.y - 10);
         }
-
-        int heightOfRectangle = ( (classInterClass.getClassContents().size() * heightOfString + 10) < 100) ? (120) : (classInterClass.getClassContents().size() * heightOfString + 10);
-
-        this.rectangle = new Rectangle(classInterClass.getPoint().x, classInterClass.getPoint().y, (int) (15 + length + 15),heightOfRectangle);
+        int proba = classInterClass.getClassContents().size() * heightOfString + 13;
+        int heightOfRectangle = ( (classInterClass.getClassContents().size() * heightOfString + 30) < 500) ? (100) : (classInterClass.getClassContents().size() * heightOfString + 13);
+        System.out.println(proba);
+        this.rectangle = new Rectangle(classInterClass.getPoint().x, classInterClass.getPoint().y, (int) (15 + length + 15),classInterClass.getPoint().y + proba);
         graphics2D.drawRect((int)rectangle.getX(),(int) rectangle.getY(),(int) rectangle.getWidth(),(int) rectangle.getHeight());
         graphics2D.drawString("C", classInterClass.getPoint().x + 5, classInterClass.getPoint().y + 15);
         graphics2D.drawString(classInterClass.getName(), classInterClass.getPoint().x + 20, classInterClass.getPoint().y + 15);
