99bedc45ab82666f9c965342e76913e2ae332183
markostojicic03 <mstojicic12923rn@raf.rs>
2023-12-08 11:00:45 +0100

    Generalizacija strelica

--- dizajn-softvera-projekat-tim_markostojicic_vidanstojic/src/main/java/raf/classycraft/app/gui/view/paint/GeneralizationPainter.java
+++ dizajn-softvera-projekat-tim_markostojicic_vidanstojic/src/main/java/raf/classycraft/app/gui/view/paint/GeneralizationPainter.java
@@ -5,6 +5,7 @@ import raf.classycraft.app.model.elementDiagram.DiagramElement;
 import raf.classycraft.app.model.elementDiagram.concreteConnections.Generalization;
 
 import java.awt.*;
+import java.awt.geom.GeneralPath;
 import java.awt.geom.Line2D;
 
 public class GeneralizationPainter extends ConnectionPainter {
@@ -18,11 +19,54 @@ public class GeneralizationPainter extends ConnectionPainter {
     public void paint(Graphics2D graphics2D, DiagramElement diagramElement) {
         if(diagramElement instanceof Connection){
             Connection connection = super.getConnection();
-            if(connection.getLine2D() == null) return;
-            if(connection.getTo() == null && connection.getFrom() == null) return;
+            if (connection.getLine2D() == null) return;
+            if (connection.getTo() == null && connection.getFrom() == null) return;
 
-            graphics2D.drawLine((int) connection.getLine2D().getX1(), (int) connection.getLine2D().getY1(), (int) connection.getLine2D().getX2(), (int) connection.getLine2D().getY2());
+            graphics2D.drawLine((int) connection.getLine2D().getX1(), (int) connection.getLine2D().getY1(),
+                    (int) connection.getLine2D().getX2(), (int) connection.getLine2D().getY2());
+
+
+            drawArrow(graphics2D, (int) connection.getLine2D().getX1(), (int) connection.getLine2D().getY1(),
+                    (int) connection.getLine2D().getX2(), (int) connection.getLine2D().getY2());
+
         }
+    }
+
+
+    private void drawArrow(Graphics2D g, int x1, int y1, int x2, int y2) {
+        int arrowSize = 15;
+
+        double angle = Math.atan2(y2 - y1, x2 - x1);
+        double sin = Math.sin(angle);
+        double cos = Math.cos(angle);
+        
+        double endX = x2;
+        double endY = y2;
+
+
+        double triangleX = endX;
+        double triangleY = endY;
+
+        double arrowPoint1X = triangleX - arrowSize * cos - arrowSize * 0.4 * sin;
+        double arrowPoint1Y = triangleY - arrowSize * sin + arrowSize * 0.4 * cos;
+
+        double arrowPoint2X = triangleX - arrowSize * cos + arrowSize * 0.4 * sin;
+        double arrowPoint2Y = triangleY - arrowSize * sin - arrowSize * 0.4 * cos;
+
+
+        GeneralPath path = new GeneralPath();
+        path.moveTo(triangleX, triangleY);
+        path.lineTo(arrowPoint1X, arrowPoint1Y);
+        path.lineTo(arrowPoint2X, arrowPoint2Y);
+        path.closePath();
+
+
+        g.setColor(Color.WHITE);
+        g.fill(path);
+
+
+        g.setColor(Color.BLACK);
+        g.draw(path);
     }
 
     @Override
