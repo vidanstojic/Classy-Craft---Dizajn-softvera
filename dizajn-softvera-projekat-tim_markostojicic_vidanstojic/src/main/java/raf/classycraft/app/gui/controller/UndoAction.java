0201947705aa26b7fe8a9c24c732e1690e6f23ef
vidanstojic <147335878+vidanstojic@users.noreply.github.com>
2023-12-26 13:26:33 +0100

    deselect kod redo i undo

--- dizajn-softvera-projekat-tim_markostojicic_vidanstojic/src/main/java/raf/classycraft/app/gui/controller/UndoAction.java
+++ dizajn-softvera-projekat-tim_markostojicic_vidanstojic/src/main/java/raf/classycraft/app/gui/controller/UndoAction.java
@@ -1,10 +1,19 @@
 package raf.classycraft.app.gui.controller;
 
+import raf.classycraft.app.gui.view.DiagramView;
 import raf.classycraft.app.gui.view.MainFrame;
+import raf.classycraft.app.gui.view.paint.ConnectionPainter;
+import raf.classycraft.app.gui.view.paint.ElementPainter;
+import raf.classycraft.app.gui.view.paint.InterClassPainter;
+import raf.classycraft.app.model.elementDiagram.Connection;
+import raf.classycraft.app.model.elementDiagram.Interclass;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UndoAction extends AbstractClassyAction{
     public UndoAction(){
@@ -16,6 +25,28 @@ public class UndoAction extends AbstractClassyAction{
     }
     @Override
     public void actionPerformed(ActionEvent e) {
+        deselect(MainFrame.getInstance().getCurrentDiagramView());
         MainFrame.getInstance().getCurrentDiagramView().getCommandManager().undoCommand();
+    }
+    private void deselect(DiagramView tempTab){
+        if(!tempTab.getListOfSelectedPainters().isEmpty()) {
+            List<ElementPainter> helpList = new ArrayList<>();
+            Interclass interclass;
+            Connection connection;
+            for (ElementPainter elementPainter : tempTab.getListOfSelectedPainters()) {
+                if (elementPainter.getRectangle() != null) {
+                    interclass = ((InterClassPainter) elementPainter).getInterclass();
+                    interclass.setColor(Color.BLACK);
+                } else if (elementPainter.getLine2D() != null) {
+                    connection = ((ConnectionPainter) elementPainter).getConnection();
+                    connection.setColor(Color.BLACK);
+                }
+                helpList.add(elementPainter);
+            }
+            for (ElementPainter elementPainter : helpList) {
+                tempTab.getListOfSelectedPainters().remove(elementPainter);
+            }
+            tempTab.repaint();
+        }
     }
 }
