b33307bd4323cf0baac810f994d2af27e174670d
markostojicic03 <mstojicic12923rn@raf.rs>
2023-12-26 23:41:42 +0100

    Serializer osnova

---
+++ dizajn-softvera-projekat-tim_markostojicic_vidanstojic/src/main/java/raf/classycraft/app/serializer/JacksonSerializer.java
@@ -0,0 +1,32 @@
+package raf.classycraft.app.serializer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import raf.classycraft.app.model.compositeImplement.Project;
+import raf.classycraft.app.serializer.Serializer;
+
+import java.io.File;
+import java.io.IOException;
+
+public class JacksonSerializer implements Serializer {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public Project loadProject(File file) {
+        try {
+            return objectMapper.readValue(file, Project.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public void saveProject(Project project) {
+        try {
+            objectMapper.writeValue(new File(String.valueOf(project.getFilepath())), project);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ dizajn-softvera-projekat-tim_markostojicic_vidanstojic/src/main/java/raf/classycraft/app/serializer/Serializer.java
@@ -0,0 +1,10 @@
+package raf.classycraft.app.serializer;
+
+import raf.classycraft.app.model.compositeImplement.Project;
+
+import java.io.File;
+
+public interface Serializer {
+    Project loadProject(File file);
+    void saveProject(Project node);
+}
