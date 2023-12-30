0bcd77d943ac6c5743d0c09cf3dc9b64a8135939
vidanstojic <147335878+vidanstojic@users.noreply.github.com>
2023-12-30 13:20:22 +0100

    ScreenShot akcija
    isto kao i za fileLoger baca exception

--- dizajn-softvera-projekat-tim_markostojicic_vidanstojic/src/main/java/raf/classycraft/app/gui/controller/ScreenshotAction.java
+++ dizajn-softvera-projekat-tim_markostojicic_vidanstojic/src/main/java/raf/classycraft/app/gui/controller/ScreenshotAction.java
@@ -1,8 +1,17 @@
 package raf.classycraft.app.gui.controller;
 
+import raf.classycraft.app.gui.view.DiagramView;
+import raf.classycraft.app.gui.view.MainFrame;
+import raf.classycraft.app.model.compositeImplement.Diagram;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class ScreenshotAction extends AbstractClassyAction{
     public ScreenshotAction(){
@@ -14,6 +23,17 @@ public class ScreenshotAction extends AbstractClassyAction{
     }
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if (!(MainFrame.getInstance().getClassyTree().getSelectedNode().getClassyNode() instanceof Diagram)) return;
+        DiagramView diagramView = MainFrame.getInstance().getCurrentDiagramView();
+        BufferedImage bufferedImage = new BufferedImage(diagramView.getWidth(), diagramView.getHeight(), BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d = bufferedImage.createGraphics();
+        diagramView.printAll(g2d);
+        g2d.dispose();
+        try {
+            ImageIO.write(bufferedImage, "png", new File("src/main/resources/image.png"));
+        } catch (IOException ex) {
+            System.out.println("lose ucitavanje");
+            throw new RuntimeException(ex);
+        }
     }
 }
